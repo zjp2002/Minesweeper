e3647e1cb25a71c21d71ad3278499c027762d237
jayzjp <1925961921@qq.com>
2021-02-23 22:54:21 +0800

    2.5.1

--- 生成exe.bat
+++ 生成exe.bat
@@ -1,3 +1,3 @@
 @echo off
-pyinstaller -i cat.ico main.py -p mineLabel.py -p minesweeper_master.py -p mineSweeperGui.py -p selfDefinedParameter.py -p statusLabel.py -p superGUI.py -p gameSettings.py -p gameAbout.py -p gameHelp.py -p gameTerms.py -p browserGUI.py -p src_help_pic_rc.py -p gameScores.py -p gameSetMoreGUI.py -p mainWindowGUI -w
+pyinstaller -i cat.ico main.py -p mineLabel.py -p minesweeper_master.py -p mineSweeperGui.py -p selfDefinedParameter.py -p statusLabel.py -p superGUI.py -p gameSettings.py -p gameAbout.py -p gameHelp.py -p gameTerms.py -p gameEntrance.py -p gameRank.py -p src_help_pic_rc.py -p gameScores.py -p gameSetMoreGUI.py -p mainWindowGUI -w
 pause
